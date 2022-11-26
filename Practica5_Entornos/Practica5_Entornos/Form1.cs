13c0526e6d0d0449d56733d94b8b2a3c1ba7e2f8
luisep92 <luis@escolano.es>
2022-11-26 20:21:50 +0100

    Arreglados errores logicos

--- Practica5_Entornos/Practica5_Entornos/Form1.cs
+++ Practica5_Entornos/Practica5_Entornos/Form1.cs
@@ -10,33 +10,42 @@ namespace Practica5_Entornos
         private void button1_Click(object sender, EventArgs e)
         {
             string textoTelegrama;
-            char tipoTelegrama = ' ';
+            char tipoTelegrama = 'o';
             int numPalabras = 0;
             double coste;
-            //Leo el telegrama
+
             textoTelegrama = txtTelegrama.Text;
-            // telegrama urgente?
+            numPalabras = Utils.CountWords(textoTelegrama);
+
             if (cbUrgente.Checked)
+            {
                 tipoTelegrama = 'u';
-            //Obtengo el número de palabras que forma el telegrama
-            numPalabras = textoTelegrama.Length;
-            //Si el telegrama es ordinario
-            if (tipoTelegrama == 'o')
+            }
+
+            if (tipoTelegrama == 'o') //ordinario
+            {
                 if (numPalabras <= 10)
-                    coste = 25;
+                    coste = 2.5;
                 else
-                    coste = 0.5 * numPalabras;
-            else
-            //Si el telegrama es urgente
-            if (tipoTelegrama == 'u')
+                    coste = 2.5 + 0.5 * (numPalabras - 10);
+            }  
+            else if (tipoTelegrama == 'u') //urgente
+            {
                 if (numPalabras <= 10)
                     coste = 5;
                 else
                     coste = 5 + 0.75 * (numPalabras - 10);
+            }
             else
+            {
                 coste = 0;
+            }
+                
             txtPrecio.Text = coste.ToString() + " euros";
+        }
 
+        private void label1_Click(object sender, EventArgs e)
+        {
 
         }
     }
---
+++ Practica5_Entornos/Practica5_Entornos/Utils.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practica5_Entornos
+{
+    internal class Utils
+    {
+        public static int CountWords(string text)
+        {
+            if (text == "")
+                return 0;
+
+            int result = 1;
+            char prev = char.MinValue;
+            foreach (char c in text)
+            {
+                if(c == ' ' && prev != ' ')
+                {
+                    result++;
+                }
+                prev = c;
+            }
+            return result;
+        }
+    }
+}
